3f5368b7354d1469458336b2dee5417e8cc59f49
sunnysidedemver <john_mayers@outlook.com>
2020-05-14 03:47:05 +0000

    removed stopwords and made all lowercase

--- mapper.py
+++ mapper.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 import sys
 
+from sklearn.feature_extraction import stop_words
+stopwords = set(stop_words.ENGLISH_STOP_WORDS)
+
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower().isalpha()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stopwords:
+            print '%s\t%s' % (word, "1")
